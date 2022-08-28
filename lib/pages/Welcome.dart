81ce44f2b6ce2e0c794276717c2b45fdf947dc24
ValerieFernandes <valerie16f@gmail.com>
2022-08-28 01:09:50 -0400

    changes to welcome

--- lib/pages/Welcome.dart
+++ lib/pages/Welcome.dart
@@ -10,43 +10,66 @@ class WelcomePage extends StatelessWidget{
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Container( 
         alignment: Alignment.center,
-        child: Column(
-    
+        
+         child: Column(
+       
          // mainAxisAlignment: MainAxisAlignment.center,
          // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Spacer(flex:4),
+            Spacer(flex:5),
 
             Image.asset('assets/logo.png', scale: 10),
 
+            Text('BlockTalk',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,),
+                ),
+            
             Spacer(flex:4),
             
             Expanded(
-              flex: 1,
+              flex: 2,
               child: TextButton(
-                style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.secondary),
+                
+                style: ButtonStyle(
+                                  minimumSize:MaterialStateProperty.all<Size>(Size(300, 50)),
+                                  backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.secondary),
                                   foregroundColor:  MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.primary)),
                 onPressed: (){
                   Navigator.push(context, MaterialPageRoute(builder: (context) =>  SignUp()));
                 },
-                child: Text('Sign Up'),
+                child: Text('Sign Up',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,),
+                          )
               )
             ),
 
-           //Spacer(flex:1),
+           Spacer(flex:1),
             Expanded(
-              flex: 1,
+              flex: 2,
               child: TextButton(
-                style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.onPrimary),
+                
+                style: ButtonStyle(minimumSize:MaterialStateProperty.all<Size>(Size(300, 60)),
+                                  backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.onPrimary),
                                   foregroundColor:  MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.primary)),
+                                  
                  onPressed: (){
                   Navigator.push(context, MaterialPageRoute(builder: (context) =>  LogIn()));
                  },
-                child: Text('Log In'),
+                child: Text('Log In',
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.primary,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,),
+                          )
               )
-            ), 
-
-            Spacer(flex:4), 
+              ),
+            Spacer(flex:3), 
           ]
         )
       ),
