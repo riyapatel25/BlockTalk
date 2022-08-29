0f712305955c9efbc71d7b9a17d9c76115662781
ValerieFernandes <valerie16f@gmail.com>
2022-08-29 19:57:07 -0400

    new top bar/post list

--- lib/Pages/PostPage.dart
+++ lib/Pages/PostPage.dart
@@ -1,99 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:block_talk/Pages/widgets/Post.dart';
-
+import 'package:google_fonts/google_fonts.dart';
+List<Post> posts = [];
 class PostPage extends StatefulWidget {
   @override
   _PostPageState createState() => _PostPageState();
 }
 
-class _PostPageState extends State<PostPage> {
+class _PostPageState extends State<PostPage> with TickerProviderStateMixin {
+  late TabController _tabController;
+    @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xFF7D5C65),
-          shadowColor: Color(0xFF808767),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              //button 1
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                    (Set<MaterialState> states) {
-                      if (states.contains(MaterialState.pressed))
-                        return Theme.of(context)
-                            .colorScheme
-                            .primary
-                            .withOpacity(0.5);
-                      return Color.fromARGB(
-                          255, 129, 111, 116); // Use the component's default.
-                    },
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'My City',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color(0xFFE9D6C4),
-                  ),
-                ),
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          shadowColor: Theme.of(context).colorScheme.tertiary,
+          title: TabBar(    
+          controller: _tabController,
+            indicatorColor: Theme.of(context).colorScheme.onPrimary,      
+            tabs:  <Widget>[
+              Tab(
+                icon: Text("My City", style: GoogleFonts.montserrat(fontSize: 20, color: Theme.of(context).colorScheme.onBackground))
               ),
-              //space
-              SizedBox(width: 10),
-              //button 2
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                    (Set<MaterialState> states) {
-                      if (states.contains(MaterialState.pressed))
-                        return Theme.of(context)
-                            .colorScheme
-                            .primary
-                            .withOpacity(0.5);
-                      return Color.fromARGB(
-                          255, 129, 111, 116); // Use the component's default.
-                    },
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  'Friends',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Color(0xFFE9D6C4),
-                  ),
-                ),
+              Tab(
+                icon: Text("My Friends", style: GoogleFonts.montserrat(fontSize: 20, color: Theme.of(context).colorScheme.onBackground))
               ),
-              // Icon(Icons.restaurant_menu),
-
-              //    Text(
-              //     'My City',
-              //     style: TextStyle(
-              //     fontSize: 20,
-              //     color: Color(0xFFE9D6C4),
-              // ),
-              //    ),
-
-              SizedBox(width: 15),
-
-              //     Text(
-              //     'Friends',
-              //     style: TextStyle(
-              //     fontSize: 20,
-              //     color: Color(0xFFE9D6C4),
-              // ),
-              //    ),
-            ],
+            ]
           ),
+        
         ),
-        body: const Post(
-          title: 'My Post',
-          rating: '4.9',
-          cookTime: '30 min',
-          thumbnailUrl:
-              'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
-        ));
+        body: 
+        TabBarView(
+          controller: _tabController,
+          children:  <Widget>[
+             Post(title: 'My Post',
+                  rating: '4.9',
+                  cookTime: '30 min',
+                  thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+                  ),
+             Post(title: 'My Post',
+                  rating: '4.9',
+                  cookTime: '30 min',
+                  thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+                  ),
+          ],     
+        ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:block_talk/Pages/PostPage.dart';
 import 'package:flutter/material.dart';
 import 'package:block_talk/Pages/LogIn.dart';
+
 // import 'package:firebase_core/firebase_core.dart';
 
 import 'Pages/Welcome.dart';
@@ -25,7 +27,7 @@ class BlockTalk extends StatelessWidget{
       title: 'BlockTalk',
       theme: ThemeData(colorScheme: colours),
       debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
+      home: PostPage(),
     );
   }
 }
