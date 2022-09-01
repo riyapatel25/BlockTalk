3228d13d9d5af6248ae8a24c1265817293b72289
ValerieFernandes <valerie16f@gmail.com>
2022-09-01 00:08:38 -0400

    fixed profile image alignment

--- lib/Pages/Profile.dart
+++ lib/Pages/Profile.dart
@@ -11,61 +11,52 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-    List<Widget> personalPosts =             
-           [ Container(height: 390, width:150, child: Align(
-              alignment: Alignment.center, child: Text(
-                '+',
-                style: TextStyle(fontSize: 55,color: Color(0xFF1B191A),),
-              ),), decoration: BoxDecoration(border: Border.all(width: 1, color:Color(0xFF1B191A)
-              ),borderRadius: BorderRadius.circular(10), ),),
+  List<Widget> personalPosts = [Container(height: 390, width:150, 
+                                          child: Align(alignment: Alignment.center, child: Text('+', style: TextStyle(fontSize: 55,color: Color(0xFF1B191A),),),), 
+                                          decoration: BoxDecoration(border: Border.all(width: 1, color:Color(0xFF1B191A)),borderRadius: BorderRadius.circular(10), ),),
 
-            Container(height: 400, child: Post(title: 'My Post',
-                  rating: '4.9',
-                  cookTime: '30 min',
-                  thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
-                  )),
-                  Container(height: 400, child: Post(title: 'My Post',
-                  rating: '4.9',
-                  cookTime: '30 min',
-                  thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
-                  )),];
+                                Container(height: 400, child: Post(title: 'My Post',
+                                      rating: '4.9',
+                                      cookTime: '30 min',
+                                      thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+                                      )),
+                                      Container(height: 400, child: Post(title: 'My Post',
+                                      rating: '4.9',
+                                      cookTime: '30 min',
+                                      thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360',
+                                      )),
+                                ];
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      
-        backgroundColor: Theme.of(context).colorScheme.onSecondary,
-        body: SafeArea(
-            child: Center(
-                //add single child view
-                child: Column(
-                   // mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-              // SizedBox(height: 20),
-              // Spacer(flex:4),
-              Text("@account_name", style: GoogleFonts.montserrat(fontSize: 24, color: Theme.of(context).colorScheme.secondary), ),
-             SizedBox(height: 5),
+      backgroundColor: Theme.of(context).colorScheme.onSecondary,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [Text("@account_name", style: GoogleFonts.montserrat(fontSize: 24, color: Theme.of(context).colorScheme.secondary),),
+                      
+                      SizedBox(height: 40),
 
-           Stack(alignment: Alignment.center, children: <Widget>[AppBar(backgroundColor: Theme.of(context).colorScheme.primary,), 
-                                  Padding( padding: EdgeInsets.fromLTRB(0, 0, 0, 150), child:Container(height:150, width: 150, child: Icon(Icons.person, size: 100,) ,
-                                            decoration: BoxDecoration(color:Theme.of(context).colorScheme.onSecondary, border: Border.all(width: 1, color: Theme.of(context).colorScheme.secondary), borderRadius: BorderRadius.circular(10),))),
-                                  ],),
-              //space
-          // SizedBox(height: 3),
-             Align(
-              alignment: Alignment.centerLeft,
-              child:Text("Posts", style: GoogleFonts.montserrat(fontSize: 24, color: Theme.of(context).colorScheme.secondary), textAlign: TextAlign.left,),),
-            
-          Container(
-          margin: const EdgeInsets.symmetric(vertical: 20.0),
-          height: 400.0,
-          child: ListView(
-            // This next line does the trick.
-            scrollDirection: Axis.horizontal,
-            children: personalPosts,
-          ),)
-             
-            ]))));
+                      Stack(alignment: Alignment.bottomCenter, children: <Widget>[AppBar(backgroundColor: Theme.of(context).colorScheme.primary,), 
+                                            Padding( padding: EdgeInsets.fromLTRB(0, 0, 0, 25), child:Container(height:150, width: 150, child: Icon(Icons.person, size: 100,) ,
+                                                      decoration: BoxDecoration(color:Theme.of(context).colorScheme.onSecondary, border: Border.all(width: 1, color: Theme.of(context).colorScheme.secondary), borderRadius: BorderRadius.circular(10),))),
+                                            ],),
+                        //space
+                      SizedBox(height: 10),
+                        Align(
+                        alignment: Alignment.centerLeft,
+                        child:Text("Posts", style: GoogleFonts.montserrat(fontSize: 24, color: Theme.of(context).colorScheme.secondary), textAlign: TextAlign.left,),),
+                      
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 20.0),
+                        height: 400.0,
+                        child: ListView( scrollDirection: Axis.horizontal, children: personalPosts),
+                      )
+                    ]
+                  )
+                )
+              )
+            );
   }
 }
